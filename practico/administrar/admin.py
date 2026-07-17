cccabe04ea2aae7fc47fc436c1c562d4a8f05933
mviteris3-star <mviteris3@unemi.edu.ec>
2026-07-16 20:46:22 -0500

    Delete practico directory

--- practico/administrar/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Contacto
-
-@admin.register(Contacto)
-class ContactoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'correo', 'telefono', 'fecha_creacion')
-    search_fields = ('nombre', 'correo', 'telefono')
-    list_filter = ('fecha_creacion',)
--- practico/administrar/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdministrarConfig(AppConfig):
-    name = 'administrar'
